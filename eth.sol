238d8ee18c54bbc605288150eb39e2dcf5323e08
Shude Li <lishude@aliyun.com>
2019-04-02 05:29:56 +0800

    Update eth.sol

--- eth.sol
+++ eth.sol
@@ -1,5 +1,50 @@
 pragma solidity ^0.5.0;
 
+contract Friendship {
+    mapping(address => address payable[]) internal friends;
+    mapping(address => mapping(address => bool)) public friendship;
+    
+    function MyFriends() public view returns (address payable[] memory) {
+        return friends[msg.sender];
+    }
+    
+    function AddFriend(address payable _friend) public {
+        if(friendship[msg.sender][_friend]){
+            return;
+        }
+        friends[msg.sender].push(_friend);
+    }
+    
+    function AddFriendList(address payable[] memory list) public {
+        for(uint i = 0; i < list.length; ++i){
+            address payable cur = list[i];
+            if(friendship[msg.sender][cur]){
+                continue;
+            }
+            friends[msg.sender].push(cur);
+            friendship[msg.sender][cur] = true;
+        }
+    }
+    
+    function DelFriend(address payable _friend) public {
+        if(!friendship[msg.sender][_friend]){
+           return;
+        }
+        delete friendship[msg.sender][_friend];
+        for(uint i = 0; i < friends[msg.sender].length; ++i){
+            if(_friend != friends[msg.sender][i]){
+                continue;
+            }
+            if(i == friends[msg.sender].length){
+                friends[msg.sender].length--;
+                return;
+            }
+            friends[msg.sender][i] = friends[msg.sender][friends[msg.sender].length-1];
+            friends[msg.sender].length--;
+        }
+    }
+}
+
 contract ERC20Interface {
     function totalSupply() public view returns (uint);
     function balanceOf(address tokenOwner) public view returns (uint balance);
@@ -12,147 +57,110 @@ contract ERC20Interface {
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
 
-
-contract Ownable {
-    address payable public owner;
+// DON'T implements ERC20Interface!
+contract TokenReciever {
+    constructor() public {}
     
-    constructor() public {
-        owner = msg.sender;
+    function transfer(address token, address to, uint value) public {
+        ERC20Interface(token).transfer(to, value);
     }
     
-    modifier OnlyOwner {
-        require(msg.sender == owner);
-        _;
+    function transferFrom(address token, address from, address to, uint tokens) public {
+        ERC20Interface(token).transferFrom(from, to, tokens);
     }
     
-    function transferOwnership(address payable _owner) public OnlyOwner {
-        owner = _owner;
-    }
-}
-
-contract Friendship is Ownable {
-    address payable[] internal friends;
-    mapping(address => bool) public friendship;
-    mapping(address => bool) public blacklist; 
-    
-    function MyFriends() public view returns (address payable[] memory) {
-        return friends;
+    function balanceOf(address token) public view returns (uint256){
+        return ERC20Interface(token).balanceOf(address(this));
     }
     
-    function AddFriend(address payable _friend) public OnlyOwner {
-        if(friendship[_friend]){
-            return;
-        }
-        friends.push(_friend);
+    function allowance(address token, address spender) public view returns (uint256) {
+        return ERC20Interface(token).allowance(address(this), spender);
     }
     
-    function AddFriendList(address payable[] memory list) public OnlyOwner {
-        for(uint i = 0; i < list.length; ++i){
-            address payable cur = list[i];
-            if(friendship[cur]){
-                continue;
-            }
-            friends.push(cur);
-            friendship[cur] = true;
-        }
-    }
-    
-    function DelFriend(address payable _friend) public OnlyOwner{
-        if(!friendship[_friend]){
-           return;
-        }
-        delete friendship[_friend];
-        for(uint i = 0; i < friends.length; ++i){
-            if(_friend != friends[i]){
-                continue;
-            }
-            if(i == friends.length){
-                friends.length--;
-                return;
-            }
-            friends[i] = friends[friends.length-1];
-            friends.length--;
-        }
-    }
-    
-    modifier OnlyUser {
-        require(!blacklist[msg.sender]);
-        _;
-    }
-    
-    function LockUser(address _user) public OnlyOwner {
-        blacklist[_user] = true;
-    }
-    
-    function UnlockUser(address _user) public OnlyOwner {
-        // blacklist[_user] = false; // 20960
-        delete blacklist[_user]; // 20872
+    function approve(address token, address spender, uint256 value) public returns (bool) {
+        return ERC20Interface(token).approve(spender, value);
     }
 }
 
-contract Tokens {
+contract Token {
+    mapping(address => TokenReciever) tokens;
+    constructor() public {}
     
+    function NewTokenReceiver() public {
+        require(address(tokens[msg.sender]) == address(0x0), "Has been created receiver address");
+        TokenReciever token = new TokenReciever();
+        tokens[msg.sender] = token;
+    }
+    
+    function WithdrawToken(address token, address _to, uint256 value) public {
+        tokens[msg.sender].transfer(token, _to, value);
+    }
 }
 
-contract RedPacket is Friendship {
-    constructor() public payable {}
-    
-    function() external payable {}
-    
-      struct Record {
+contract RedPackage is Friendship, Token {
+     struct Record {
+       address owner;
        bool equalDivision;
        bool onlyFriend;
-       address token; // 0x0 for ETH
-       uint256 value; // value for everyone NOT all amount
+       address token;
+       uint256 amount;
        uint256 remainAmount;
        uint256 size;
        uint256 remainSize;
        uint256 timestamp;
-       uint256 expireDays;
+       uint256 expired;
        address payable[] grabbed;
     }
-
+    
     mapping(string => Record) records;
     
-    function Giving(string memory passwd,
-                    bool equalDivision,
-                    bool onlyFriend,
-                    address token,
-                    uint256 value,
-                    uint256 size,
-                    uint256 expireDays) public payable OnlyOwner {
-        require(value > 0 && size > 0, "giving value and package size should be greater than 0");
-        uint256 amount = value * size;
-        if(token == address(0x0)){
-            require(address(this).balance >= amount, "not sufficient funds");
-        } else {
-            require(ERC20Interface(token).balanceOf(address(this)) >= amount, "not sufficient funds");
+    function IsWordExists(string memory word) public view returns (bool) {
+         Record memory r = records[word];
+         return r.owner != address(0x0);
+    }
+    
+    function Giving(string memory word, bool equalDivision, bool onlyFriend, uint256 size, uint256 expireDays) public payable {
+        require(size > 0 && msg.value > 0 && msg.value > size && expireDays > 0, "invalid data provided");
+        require(!IsWordExists(word), "Red package exists");
+        
+        if(equalDivision) {
+            require(msg.value % size == 0, "Invalid value and size for equal division mode");
         }
         
-        address payable[] memory grabbed; 
-        records[passwd] =  Record(onlyFriend, equalDivision, token, value, amount, size, size, block.timestamp, expireDays, grabbed);
+        address payable[] memory grabbed;
+        records[word] = Record(msg.sender, equalDivision, onlyFriend, address(0x0), msg.value, msg.value, size, size, now, now + expireDays * 1 days, grabbed);
     }
     
-    function Revoke(string memory passwd) public OnlyOwner {
-        delete records[passwd];
+    function Giving(string memory word, address token, uint256 value, bool equalDivision, bool onlyFriend, uint256 size, uint256 expireDays) public {
+        uint256 balance = tokens[msg.sender].balanceOf(token);
+        require(balance >= value, "not sufficient funds");
+        require(size > 0 && value > 0 && value > size && expireDays > 0, "invalid data provided");
+        require(!IsWordExists(word), "Red package exists");
+        
+        if(equalDivision) {
+            require(value % size == 0, "Invalid value and size for equal division mode");
+        }
+        
+        address payable[] memory grabbed;
+        records[word] = Record(msg.sender, equalDivision, onlyFriend, token, value, value, size, size, now, now + expireDays * 1 days, grabbed);
     }
     
-    function Grabbing(string memory passwd) public {
-        require(!blacklist[msg.sender]);
-        Record storage r = records[passwd];
-        require(r.remainAmount != 0 && r.remainSize != 0 && r.timestamp + r.expireDays <= block.timestamp, "invalid red package password or expired");
+    function Grabbing(string memory word) public {
+        Record storage r = records[word];
+        require(r.owner != address(0x0), "Red package not exists");
+        require(r.expired <= now, "Red package expired");
         
         if(r.onlyFriend) {
-            require(friendship[msg.sender], "only friend can grab this red package");
+            require(friendship[r.owner][msg.sender], "Only friend can grab");
         }
         
         for(uint256 i = 0; i < r.grabbed.length; ++i){
-            require(msg.sender != r.grabbed[i], "only grabbed once");
+            require(msg.sender != r.grabbed[i], "can't grabbed twice");
         }
         
         uint256 value = 0;
         if(r.equalDivision) {
-            value = r.value;
+            value = uint256(r.amount) / uint256(r.size);
         } else  if (r.remainSize == 1){
             value = r.remainAmount;
         } else {
@@ -168,25 +176,20 @@ contract RedPacket is Friendship {
                 value = tmp;
             }
         }
-
+        
         if(r.token == address(0x0)){
             msg.sender.transfer(value);
         } else {
-            ERC20Interface(r.token).transfer(msg.sender, value);
+            tokens[r.owner].transfer(r.token, msg.sender, value);
         }
-        
+
+
         r.remainAmount -= value;
         r.remainSize--;
-        
         if(r.remainSize == 0){
-            delete records[passwd];
+            delete records[word];
+            return;
         }
-        
         r.grabbed.push(msg.sender);
-    }
-  
-    function destructor() public OnlyOwner {
-        selfdestruct(owner);
-    }
+     }
 }
-
